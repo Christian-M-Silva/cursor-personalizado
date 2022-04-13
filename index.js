1c29cab953ec0adf43ebd32e750ec6051c194d66
Christian-M-Silva <hoodprogrammer@gmail.com>
2022-04-13 09:18:27 -0300

    Correção do scroll e inicio da estilização do efeito hover nos span

--- index.js
+++ index.js
@@ -8,16 +8,15 @@ body.addEventListener('mousemove', getCoordinate) //adicionando o evento de move
 function getCoordinate(event) {
     let x = event.clientX
     let y = event.clientY
-    let screenY = event.screenY
-    let screenX = event.screenX     //Antes de continuar ver a possibilidade de em vez de usar o if para isso ver as propriedades do onscroll para quando ele for ativado desabilitar o movimento da bolinha
+        //Antes de continuar ver a possibilidade de em vez de usar o if para isso ver as propriedades do onscroll para quando ele for ativado desabilitar o movimento da bolinha
     
     //Pegando as propriedades do event, os dois primeiro é para coordenada e os dois últimos para impedir que a bolinha ultrapasse a tela e assim não crie um scroll
 
     //console.log(event)
 
-    if (screenY <= 694 && screenX <= 1336) {
-        circle.style.transform = `translate(${x - 25}px,${y -20}px)`
-    } 
+    
+        circle.style.transform = `translate(${x - 13}px,${y -7}px)`
+    
     //condição que vai filtrar se a bolinha está dentro das coordenadas para não criar scroll sendo positivo ele vai passar para o circulo a coordenada do mouse e assim vai passar para o estilo do circle o translate com que contem as coordenadas do mouse ajustadas.
 
 
