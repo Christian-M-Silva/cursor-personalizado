cdf77765058d195b91c121e3eeacdce6ad6dbd91
Christian-M-Silva <hoodprogrammer@gmail.com>
2022-04-30 16:27:02 -0300

    Update

--- index.js
+++ index.js
@@ -1,33 +1,41 @@
 const body = document.querySelector('body')
-const circle = document.querySelector('.circle')
+const divEffect = document.querySelector('.div-effect')
+const circleActive = document.querySelector('.circleActive')
 const cms = document.querySelector('.cms')
-//Pegando tanto o elemento body como o elemento que tem a classe circle e cms
+const emphasis = document.getElementsByClassName("emphasis")
 
 body.addEventListener('mousemove', getCoordinate) //adicionando o evento de mover o mouse no body e passando a função pegar coordenada que está abaixo
-
-//cms.addEventListener('mouseover', getHover)
-//cms.addEventListener('mouseout', dropHover) adicionei esses dois eventos
 
 function getCoordinate(event) {
     let x = event.clientX
     let y = event.clientY
-    //Pegando as propriedades do event, os dois primeiro é para a coordenada
-    //console.log(event)
-
-    circle.style.transform = `translate(${x - 13}px,${y -7}px)`
+    divEffect.style.transform = `translate(${x}px,${y}px)`
+    //Pegando as propriedades do event, os dois primeiro é para a coordenada e ai os valores que eu pego eu uso como coordenadas do mouse
 }
 
-/*function getHover(event) {
-    console.log(event)
-    if (event.isTrusted == true) {
-        circle.style.width = "50px"
-    } Nesse caso eu tentei pegar uma propriedade de quando o mouse passa por cima e assim para identificar quando o mouse está hover e poder aumentar a bolinha (circle)
- }*/
+cms.addEventListener('mouseover', active) 
+cms.addEventListener('mouseleave', disabled)//adicionando o evento de passar o mouse por cima do cms que é meu link do git e passando a função active e abaixo o evento tirar o mouse de cima com a função disabled
 
- /*function dropHover(event) {
-    if (event.isTrusted == true) {
-        circle.style.width = "30px"
-    } Nesse caso eu tentei pegar uma propriedade de quando o mouse sai de cima do elemento para fazer ele voltar ao normal
- }*/
+function active() {
+    divEffect.classList.toggle('circle')
+    divEffect.classList.toggle('circleActive')
+    //Aqui quando passo o mouse por cima e ativa essa função eu retiro a classe circle e adiciono a circleActive
+}
+function disabled() {
+    divEffect.classList.toggle('circle')
+    divEffect.classList.toggle('circleActive')
+    //Aqui quando eu tiro o mouse por cima e ativa essa função eu retiro a classe circleActive e adiciono a circle.
+}
 
- 
+console.log(emphasis)
+
+emphasis[0].addEventListener('mouseover', on) 
+emphasis[0].addEventListener('mouseleave', off)
+
+function on() {
+    divEffect.classList.add('circleOn')
+}
+
+function off() {
+    divEffect.classList.remove('circleOn')
+}
