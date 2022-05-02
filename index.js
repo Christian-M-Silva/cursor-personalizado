704e8e4641310ea1feb80a385bd91dd0bb3465a3
Christian-M-Silva <hoodprogrammer@gmail.com>
2022-05-02 06:42:04 -0300

    Finalização do desktop

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ body.addEventListener('mousemove', getCoordinate) //adicionando o evento de move
 function getCoordinate(event) {
     let x = event.clientX
     let y = event.clientY
-    divEffect.style.transform = `translate(${x}px,${y}px)`
+    divEffect.style.transform = `translate(${x + 5}px,${y - 60}px)`
     //Pegando as propriedades do event, os dois primeiro é para a coordenada e ai os valores que eu pego eu uso como coordenadas do mouse
 }
 
@@ -29,13 +29,245 @@ function disabled() {
 
 console.log(emphasis)
 
-emphasis[0].addEventListener('mouseover', on) 
-emphasis[0].addEventListener('mouseleave', off)
+emphasis[0].addEventListener('mouseover', batmanOn) 
+emphasis[0].addEventListener('mouseleave', batmanOff)
+emphasis[1].addEventListener('mouseover', mattOn) 
+emphasis[1].addEventListener('mouseleave', mattOff)
+emphasis[2].addEventListener('mouseover', robertOn) 
+emphasis[2].addEventListener('mouseleave', robertOff)
+emphasis[3].addEventListener('mouseover', homemOn) 
+emphasis[3].addEventListener('mouseleave', homemOff)
+emphasis[4].addEventListener('mouseover', gothamOn) 
+emphasis[4].addEventListener('mouseleave', gothamOff)
+emphasis[5].addEventListener('mouseover', pinguimOn) 
+emphasis[5].addEventListener('mouseleave', pinguimOff)
+emphasis[6].addEventListener('mouseover', colinOn) 
+emphasis[6].addEventListener('mouseleave', colinOff)
+emphasis[7].addEventListener('mouseover', carmineOn) 
+emphasis[7].addEventListener('mouseleave', carmineOff)
+emphasis[8].addEventListener('mouseover', johnOn) 
+emphasis[8].addEventListener('mouseleave', johnOff)
+emphasis[9].addEventListener('mouseover', bruceOn) 
+emphasis[9].addEventListener('mouseleave', bruceOff)
+emphasis[10].addEventListener('mouseover', selinaOn) 
+emphasis[10].addEventListener('mouseleave', selinaOff)
+emphasis[11].addEventListener('mouseover', zoeOn) 
+emphasis[11].addEventListener('mouseleave', zoeOff)
+emphasis[12].addEventListener('mouseover', mulherOn) 
+emphasis[12].addEventListener('mouseleave', mulherOff)
+emphasis[13].addEventListener('mouseover', jimOn) 
+emphasis[13].addEventListener('mouseleave', jimOff)
+emphasis[14].addEventListener('mouseover', jeffreyOn) 
+emphasis[14].addEventListener('mouseleave', jeffreyOff)
+emphasis[15].addEventListener('mouseover', alfredOn) 
+emphasis[15].addEventListener('mouseleave', alfredOff)
+emphasis[16].addEventListener('mouseover', andyOn) 
+emphasis[16].addEventListener('mouseleave', andyOff)
+emphasis[17].addEventListener('mouseover', charadaOn) 
+emphasis[17].addEventListener('mouseleave', charadaOff)
+emphasis[18].addEventListener('mouseover', paulOn) 
+emphasis[18].addEventListener('mouseleave', paulOff)
+emphasis[19].addEventListener('mouseover', cavaleirOn) 
+emphasis[19].addEventListener('mouseleave', cavaleirooff)
 
-function on() {
+
+
+function batmanOn() {
     divEffect.classList.add('circleOn')
+    divEffect.classList.add('batman')
 }
 
-function off() {
+function batmanOff() {
     divEffect.classList.remove('circleOn')
+    divEffect.classList.remove('batman')
+}
+
+function mattOn() {
+    divEffect.classList.add('circleOn')
+    divEffect.classList.add('matt')
+}
+
+function mattOff() {
+    divEffect.classList.remove('circleOn')
+    divEffect.classList.remove('matt')
+}
+
+function robertOn() {
+    divEffect.classList.add('circleOn')
+    divEffect.classList.add('robert')
+}
+
+function robertOff() {
+    divEffect.classList.remove('circleOn')
+    divEffect.classList.remove('robert')
+}
+
+function homemOn() {
+    divEffect.classList.add('circleOn')
+    divEffect.classList.add('homem')
+}
+
+function homemOff() {
+    divEffect.classList.remove('circleOn')
+    divEffect.classList.remove('homem')
+}
+
+function gothamOn() {
+    divEffect.classList.add('circleOn')
+    divEffect.classList.add('gotham')
+}
+
+function gothamOff() {
+    divEffect.classList.remove('circleOn')
+    divEffect.classList.remove('gotham')
+}
+
+function pinguimOn() {
+    divEffect.classList.add('circleOn')
+    divEffect.classList.add('pinguim')
+}
+
+function pinguimOff() {
+    divEffect.classList.remove('circleOn')
+    divEffect.classList.remove('pinguim')
+}
+
+function colinOn() {
+    divEffect.classList.add('circleOn')
+    divEffect.classList.add('colin')
+}
+
+function colinOff() {
+    divEffect.classList.remove('circleOn')
+    divEffect.classList.remove('colin')
+}
+
+function carmineOn() {
+    divEffect.classList.add('circleOn')
+    divEffect.classList.add('carmine')
+}
+
+function carmineOff() {
+    divEffect.classList.remove('circleOn')
+    divEffect.classList.remove('carmine')
+}
+
+function johnOn() {
+    divEffect.classList.add('circleOn')
+    divEffect.classList.add('john')
+}
+
+function johnOff() {
+    divEffect.classList.remove('circleOn')
+    divEffect.classList.remove('john')
+}
+
+function bruceOn() {
+    divEffect.classList.add('circleOn')
+    divEffect.classList.add('bruce')
+}
+
+function bruceOff() {
+    divEffect.classList.remove('circleOn')
+    divEffect.classList.remove('bruce')
+}
+
+function selinaOn() {
+    divEffect.classList.add('circleOn')
+    divEffect.classList.add('selina')
+}
+
+function selinaOff() {
+    divEffect.classList.remove('circleOn')
+    divEffect.classList.remove('selina')
+}
+
+function zoeOn() {
+    divEffect.classList.add('circleOn')
+    divEffect.classList.add('zoe')
+}
+
+function zoeOff() {
+    divEffect.classList.remove('circleOn')
+    divEffect.classList.remove('zoe')
+}
+
+function mulherOn() {
+    divEffect.classList.add('circleOn')
+    divEffect.classList.add('mulher')
+}
+
+function mulherOff() {
+    divEffect.classList.remove('circleOn')
+    divEffect.classList.remove('mulher')
+}
+
+function jimOn() {
+    divEffect.classList.add('circleOn')
+    divEffect.classList.add('jim')
+}
+
+function jimOff() {
+    divEffect.classList.remove('circleOn')
+    divEffect.classList.remove('jim')
+}
+
+function jeffreyOn() {
+    divEffect.classList.add('circleOn')
+    divEffect.classList.add('jeffrey')
+}
+
+function jeffreyOff() {
+    divEffect.classList.remove('circleOn')
+    divEffect.classList.remove('jeffrey')
+}
+
+function alfredOn() {
+    divEffect.classList.add('circleOn')
+    divEffect.classList.add('alfred')
+}
+
+function alfredOff() {
+    divEffect.classList.remove('circleOn')
+    divEffect.classList.remove('alfred')
+}
+
+function andyOn() {
+    divEffect.classList.add('circleOn')
+    divEffect.classList.add('andy')
+}
+
+function andyOff() {
+    divEffect.classList.remove('circleOn')
+    divEffect.classList.remove('andy')
+}
+
+function charadaOn() {
+    divEffect.classList.add('circleOn')
+    divEffect.classList.add('charada')
+}
+
+function charadaOff() {
+    divEffect.classList.remove('circleOn')
+    divEffect.classList.remove('charada')
+}
+
+function paulOn() {
+    divEffect.classList.add('circleOn')
+    divEffect.classList.add('paul')
+}
+
+function paulOff() {
+    divEffect.classList.remove('circleOn')
+    divEffect.classList.remove('paul')
+}
+
+function cavaleirOn() {
+    divEffect.classList.add('circleOn')
+    divEffect.classList.add('cavaleiro')
+}
+
+function cavaleirooff() {
+    divEffect.classList.remove('circleOn')
+    divEffect.classList.remove('cavaleiro')
 }
